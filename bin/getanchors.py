ef237e8eeeaba5d8b4af9a502f3965b491974f49
John Kristoff <jtkristoff@users.noreply.github.com>
2019-06-07 11:25:15 -0500

    puts anchors into a db

--- bin/getanchors.py
+++ bin/getanchors.py
@@ -1,11 +1,17 @@
 #!/usr/bin/env python3
 
 import requests
+import psycopg2
 import sys
+
+from psycopg2.extensions import AsIs
 
 url  = 'https://atlas.ripe.net/api/v2/anchors/'
 anchors = 0
 max_anchors = 1000
+
+conn = psycopg2.connect("dbname=rssmon")
+cur  = conn.cursor()
 
 # max_anchors might exceed threshold, but we won't fetch pages indefinitely
 while url != None and anchors <= max_anchors:
@@ -18,14 +24,33 @@ while url != None and anchors <= max_anchors:
     data = resp.json()
 
     for anchor in data['results']:
-        sys.stdout.write("id: %d|country: %s|ip_v4: %s"
-            % (anchor['id'], anchor['country'], anchor['ip_v4']))
+        if anchor['is_disabled'] == True:
+            continue
+
+        columns = []
+        columns.extend(['id','probe','country','city','ip_v4','as_v4'])
         if anchor['ip_v6'] == None:
-            sys.stdout.write("\n")
+            pass
         else:
-            sys.stdout.write("|ip_v6: %s\n" % (anchor['ip_v6']))
+            columns.extend(['ip_v6','as_v6'])
+
+        insert_statement = 'INSERT INTO anchor (%s) values %s'
+        values  = [anchor[column] for column in columns]
+
+        # saw a v6addr with leading space, so we hack
+        for index, value in enumerate(values):
+            if isinstance(value, str):
+                values[index] = value.strip()
+
+        cur.execute(insert_statement, (AsIs(','.join(columns)), tuple(values)))
+
         anchors += 1
 
     url = data['next']
 
+conn.commit()
+cur.close()
+conn.close()
+
+# TODO: send this to syslog
 sys.stdout.write("number of anchors: %d\n" % (anchors))
